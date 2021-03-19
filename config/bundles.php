840421183e56f2a3c824fc6351cd88eaca5f5b79
Idrissa Ndiouck <wduunx7@gmail.com>
2021-03-19 16:21:56 +0000

    HomeController, fichier twig

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
